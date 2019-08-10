0c03c5a44ac1e1a5b78a98aa872419d1e218727b
bchachaj <benedict.chachaj@student.fairfield.edu>
2019-08-10 15:29:40 -0400

    dependencies

---
+++ index.js
@@ -0,0 +1,14 @@
+const express = require('express');
+
+const app = express();
+
+const port = 3002;
+
+app.get('/', (req, res) => {
+   res.send('Hello'); 
+});
+
+
+app.listen(port, () => {
+    console.log(`listening on ${port}`);
+});
