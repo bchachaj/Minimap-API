13b553c4022f4a1e9bd566ff7ae36910bb55fb8d
bchachaj <benedict.chachaj@student.fairfield.edu>
2019-08-10 18:00:08 -0400

    sort out encryption

--- models/User.js
+++ models/User.js
@@ -1,16 +1,52 @@
 const mongoose = require('mongoose');
+const bcrypt = require('bcrypt');
 
-const userSchema = new mongoose.Schema({ 
+const userSchema = new mongoose.Schema({
     email: {
-        type: String, 
+        type: String,
         required: true,
-        unique: true 
+        unique: true
     },
     password: {
         type: String,
         required: true
     }
 });
+
+userSchema.pre('save', function (next) {
+    const user = this;
+
+    if (!user.isModified('password')) {
+        return next();
+    }
+
+    bcrypt.genSalt(10, (err, salt) => {
+        if (err) return next(err);
+
+        bcrypt.hash(user.password, salt, (err, hash) => {
+            if (err) return next(err);
+
+            user.password = hash;
+            next();
+        });
+    })
+
+});
+
+userSchema.methods.comparePassword = function(passCandidate) {
+    return new Promise((resolve, reject) => {
+
+        console.log(passCandidate, this)
+        bcrypt.compare(passCandidate, this.password, (err, isMatch) => {
+            if (err) {
+                console.log('not match')
+                return reject(err);
+            } else {
+                return resolve(true);
+            }
+        });
+    });
+}
 
 
 mongoose.model('User', userSchema);
--- routes/authRouter.js
+++ routes/authRouter.js
@@ -6,38 +6,48 @@ const jwt = require('jsonwebtoken');
 
 const User = mongoose.model('User');
 
-const { JWT_SECRET } = process.env; 
+const { JWT_SECRET } = process.env;
 
 router.post('/signup', async (req, res) => {
-    const { email, password } = req.body; 
+    const { email, password } = req.body;
+    console.log(email, password)
     const user = new User({ email, password });
-
+    console.log(user);
     try {
         await user.save();
-        const token = jwt.sign({ userId: user._id }, JWT_SECRET) 
-
+        const token = jwt.sign({ userId: user._id }, JWT_SECRET)
         res.send({ token });
-    } catch (err) {
-        return res.status(422).send(err.message)        
-    }
 
-});
-
-router.post('/login', async (req, res) => {
-    const { email, password } = req.body; 
-    const user = new User({ email, password });
-
-    try {
-        await user.save();
-        res.send('post signup request');
     } catch (err) {
         return res.status(422).send(err.message)
     }
 
 });
 
+router.post('/login', async (req, res) => {
+    const { email, password } = req.body;
+    console.log(email, password)
+    if (!email || !password) return res.status(422).send({ error: 'Credentials cannot be blank' });
+
+    const user = await User.findOne({ email });
+
+    if (!user) {
+        return res.status(404).send({ error: `Invalid credentials` });
+    }
+
+    try {
+        await user.comparePassword(password);
+        const token = jwt.sign({ userId: user._id }, JWT_SECRET);
+        res.send({ token })
+    } catch (err) {
+        console.log(err);
+        return res.status(422).send({ error: 'Invalid credentials' });
+    }
+
+});
+
 router.post('/logout', async (req, res) => {
-    const { email, password } = req.body; 
+    const { email, password } = req.body;
     const user = new User({ email, password });
 
     await user.save();
