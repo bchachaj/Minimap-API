6cb10727c5b7e08421a76840b91031ddde032b48
bchachaj <benedict.chachaj@student.fairfield.edu>
2019-08-10 15:56:42 -0400

    connect to mongo

--- index.js
+++ index.js
@@ -1,12 +1,33 @@
+require('dotenv').config()
 const express = require('express');
+const mongoose = require('mongoose');
 
 const app = express();
-
 const port = 3002;
 
 app.get('/', (req, res) => {
    res.send('Hello'); 
 });
+
+const { DB_USER, DB_PASSWORD } = process.env; 
+
+const mongoUri = `mongodb+srv://${DB_USER}:${DB_PASSWORD}@minimap-dtrf0.mongodb.net/test?retryWrites=true&w=majority`;
+
+mongoose.connect(mongoUri, {
+    useNewUrlParser: true, 
+    useCreateIndex: true 
+});
+
+
+mongoose.connection.on('connected', () => {
+    console.log('connected to mongod');
+});
+
+mongoose.connection.on('error', (err) => {
+    console.error('Error connecting to mongod', err);
+})
+
+console.log(mongoUri);
 
 
 app.listen(port, () => {
