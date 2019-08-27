bc68536270a8171e91a286077df6df97d832ce3e
bchachaj <benedict.chachaj@student.fairfield.edu>
2019-08-26 21:37:56 -0400

    setting correct headers for presigned url gen

--- image_process/index.js
+++ image_process/index.js
@@ -1,33 +1,37 @@
 require('dotenv').config();
 
 var aws = require('aws-sdk')
-var uuid = require('uuid');
 
 aws.config.update({
     secretAccessKey: process.env.AWS_SECRET_KEY,
     accessKeyId: process.env.AWS_ACCESS_ID,
     region: process.env.AWS_REGION,
-    keyPrefix: process.env.AWS_KEYPREFIX
 });
 
 var s3 = new aws.S3();
 
+
 function generateUploadURL({ fileName }) {
-    var params = { Bucket: process.env.AWS_BUCKET, Key: fileName, ContentType: 'image/jpeg' };
+
+    var params = { 
+        Bucket: process.env.AWS_BUCKET, 
+        Key: fileName, 
+        ContentType: 'image/jpeg',
+        ACL: 'public-read'
+     };
 
     return new Promise((resolve, reject) => {
         s3.getSignedUrl('putObject', params, function (err, url) {
-        if(err) {
-            reject(err);
-        } else {
-            console.log('Your generated pre-signed URL is', url);
-            resolve(url); 
-        }
-        
-    }); 
+            if (err) {
+                reject(err);
+            } else {
+                console.log('Your generated pre-signed URL is', url);
+                resolve(url);
+            }
+        });
     });
 
-  
+
 }
 
 
